0abda2ac705bcd3d0cf89a758c7a85adaafaab98
JD Paul <jdpaul123@gmail.com>
2021-09-07 15:56:36 -0700

    Made the UI pretty black

--- UOiOSDevelopmentClub/UOiOSDevelopmentClub/Helpers/Styling.swift
+++ UOiOSDevelopmentClub/UOiOSDevelopmentClub/Helpers/Styling.swift
@@ -28,7 +28,7 @@ class Utilities {
     
     static func styleFilledButton(_ button: UIButton) {
         // Filled rounded corner style
-        button.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1)
+        button.backgroundColor = UIColor.green //UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1)
         button.layer.cornerRadius = 25.0
         button.tintColor = UIColor.white
     }
@@ -36,7 +36,7 @@ class Utilities {
     static func styleHollowButton(_ button: UIButton) {
         // Hollow rounded corner style
         button.layer.borderWidth = 2
-        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.borderColor = UIColor.green.cgColor
         button.layer.cornerRadius = 25.0
         button.tintColor = UIColor.black
     }
