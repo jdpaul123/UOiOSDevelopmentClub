524e875127a5acd3d9d5fc73fdc0fc0007af6f04
JD Paul <jdpaul123@gmail.com>
2021-09-11 13:54:27 -0700

    performed pod deinitiation and got rid of the .xcworkspace to initiate the transfer to Swift Package Manager

--- UOiOSDevelopmentClub/Pods/Target Support Files/Pods-UOiOSDevelopmentClub-UOiOSDevelopmentClubUITests/Pods-UOiOSDevelopmentClub-UOiOSDevelopmentClubUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_UOiOSDevelopmentClub_UOiOSDevelopmentClubUITests : NSObject
-@end
-@implementation PodsDummy_Pods_UOiOSDevelopmentClub_UOiOSDevelopmentClubUITests
-@end
--- UOiOSDevelopmentClub/Pods/Target Support Files/Pods-UOiOSDevelopmentClub/Pods-UOiOSDevelopmentClub-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_UOiOSDevelopmentClub : NSObject
-@end
-@implementation PodsDummy_Pods_UOiOSDevelopmentClub
-@end
